722158d8b306253b1024cf6970a9cbe3cab364c3
EJIHYE <118796429+EJIHYE@users.noreply.github.com>
2022-12-30 15:58:15 +0900

    Update 문제 20.py

--- 엄지혜/문제 20.py
+++ 엄지혜/문제 20.py
@@ -30,20 +30,20 @@ import random                                     #random 모듈 사용
 m = int(input("input = "))                        #m에 input을 이용해 외부값을 받고 이를 숫자로 변환하여 저장
 n = m                                             #n에 m값 저장
 
-mn = [[random.choice(['.','.','.','.','*']) for x in range(n)] for y in range(m)] #mn에 random으로 
-for y in mn:                                      #
-    print(''.join(y))                             #
-r = mn.copy()                                     #
-for y, yd in enumerate(r):
-    for x, xd in enumerate(yd):
-        if r[y][x] == '*': continue
-        count = 0
-        c = [[''] if y - 1 < 0 else r[y-1][0 if x - 1 < 0 else x - 1:x+2],
-             r[y][0 if x - 1 < 0 else x - 1:x+2],             
-             [''] if y + 1 >= m else r[y+1][0 if x - 1 < 0 else x - 1:x+2]]
-        for z in c:
-            count+=z.count('*')
-        r[y][x] = str(count)
+mn = [[random.choice(['.','.','.','.','*']) for x in range(n)] for y in range(m)] #mn에 random으로 리스트 중 하나를 랜덤으로 선택/for문을 이용해 m번만큼 반복
+for y in mn:                                      #for문을 이용하여 반복
+    print(''.join(y))                             #결과 프린트
+r = mn.copy()                                     #mn 카피하여 r에 저장
+for y, yd in enumerate(r):                        #for문을 이용하여 반복
+    for x, xd in enumerate(yd):                   #for문을 이용하여 반복
+        if r[y][x] == '*': continue               #if문을 이용해 r[y][x]가 *과 같다면
+        count = 0                                 #count 0으로 저장
+        c = [[''] if y - 1 < 0 else r[y-1][0 if x - 1 < 0 else x - 1:x+2],   #c에 리스트를 저장/if와 else를 통해 구현
+             r[y][0 if x - 1 < 0 else x - 1:x+2],                            #c에 리스트를 저장/if와 else를 통해 구현
+             [''] if y + 1 >= m else r[y+1][0 if x - 1 < 0 else x - 1:x+2]]  #c에 리스트를 저장/if와 else를 통해 구현
+        for z in c:                               #for문을 이용하여 반복
+            count+=z.count('*')                   #z에 *을 세서 더하고 count에 저장
+        r[y][x] = str(count)                      #count값을 문자형을 변환하여 저장
 
 #----------------------------------------------
 #과제 2. 행과 열을 입력하면 '*'과 '.'으로 구성된 행렬을 출력하시오.
@@ -57,25 +57,27 @@ for y, yd in enumerate(r):
 #        ['.', '.', '.', '*', '.', '*']]
 
 #과제 2 풀이
-import random
-m = int(input("M:"))
-n = int(input("N:"))
-if not(n > 0 and m <= 100):
+import random                                     #random 모듈 사용
+m = int(input("row = "))                          #m에 input을 이용해 외부값을 받고 이를 숫자로 변환하여 저장
+n = int(input("column = "))                       #n에 input을 이용해 외부값을 받고 이를 숫자로 변환하여 저장
+
+if not(n > 0 and m <= 100):                       #조건2를 위해 범위 지정/1보다 크고 100이하로 범위 지정
     raise Exception("N > 0, M <= 100")
-mn = [[random.choice(['.','.','.','.','*']) for x in range(n)] for y in range(m)]
-for y in mn:
-    print(''.join(y))
-r = mn.copy()
-for y, yd in enumerate(r):
-    for x, xd in enumerate(yd):
-        if r[y][x] == '*': continue
-        count = 0
-        c = [[''] if y - 1 < 0 else r[y-1][0 if x - 1 < 0 else x - 1:x+2],
-             r[y][0 if x - 1 < 0 else x - 1:x+2],             
-             [''] if y + 1 >= m else r[y+1][0 if x - 1 < 0 else x - 1:x+2]]
-        for z in c:
-            count+=z.count('*')
-        r[y][x] = str(count)
+    
+mn = [[random.choice(['.','.','.','.','*']) for x in range(n)] for y in range(m)] #mn에 random으로 리스트 중 하나를 랜덤으로 선택/for문을 이용해 m번만큼 반복
+for y in mn:                                      #for문을 이용하여 반복
+    print(''.join(y))                             #결과 프린트
+r = mn.copy()                                     #mn 카피하여 r에 저장
+for y, yd in enumerate(r):                        #for문을 이용하여 반복
+    for x, xd in enumerate(yd):                   #for문을 이용하여 반복
+        if r[y][x] == '*': continue               #if문을 이용해 r[y][x]가 *과 같다면
+        count = 0                                 #count 0으로 저장
+        c = [[''] if y - 1 < 0 else r[y-1][0 if x - 1 < 0 else x - 1:x+2],   #c에 리스트를 저장/if와 else를 통해 구현
+             r[y][0 if x - 1 < 0 else x - 1:x+2],                            #c에 리스트를 저장/if와 else를 통해 구현
+             [''] if y + 1 >= m else r[y+1][0 if x - 1 < 0 else x - 1:x+2]]  #c에 리스트를 저장/if와 else를 통해 구현
+        for z in c:                               #for문을 이용하여 반복
+            count+=z.count('*')                   #z에 *을 세서 더하고 count에 저장
+        r[y][x] = str(count)                      #count값을 문자형을 변환하여 저장
 
 #----------------------------------------------
 #과제 3. 과제2에서 생성한 행렬을 이용하여 지뢰(*)를 제외한 나머지 격자의 숫자값을 찾고 행렬을 출력하시오.
@@ -89,27 +91,30 @@ for y, yd in enumerate(r):
 #        012*2*
 
 #과제 3 풀이
-import random
-m = int(input("M:"))
-n = int(input("N:"))
-if not(n > 0 and m <= 100):
+import random                                     #random 모듈 사용
+m = int(input("row = "))                          #m에 input을 이용해 외부값을 받고 이를 숫자로 변환하여 저장
+n = int(input("column = "))                       #n에 input을 이용해 외부값을 받고 이를 숫자로 변환하여 저장
+
+if not(n > 0 and m <= 100):                       #조건2를 위해 범위 지정/1보다 크고 100이하로 범위 지정
     raise Exception("N > 0, M <= 100")
-mn = [[random.choice(['.','.','.','.','*']) for x in range(n)] for y in range(m)]
 
-r = mn.copy()
-for y, yd in enumerate(r):
-    for x, xd in enumerate(yd):
-        if r[y][x] == '*': continue
-        count = 0
-        c = [[''] if y - 1 < 0 else r[y-1][0 if x - 1 < 0 else x - 1:x+2],
-             r[y][0 if x - 1 < 0 else x - 1:x+2],             
-             [''] if y + 1 >= m else r[y+1][0 if x - 1 < 0 else x - 1:x+2]]
-        for z in c:
-            count+=z.count('*')
-        r[y][x] = str(count)
+mn = [[random.choice(['.','.','.','.','*']) for x in range(n)] for y in range(m)] #mn에 random으로 리스트 중 하나를 랜덤으로 선택/for문을 이용해 m번만큼 반복
+for y in mn:                                      #for문을 이용하여 반복
+    print(''.join(y))                             #결과 프린트
+r = mn.copy()                                     #mn 카피하여 r에 저장
+for y, yd in enumerate(r):                        #for문을 이용하여 반복
+    for x, xd in enumerate(yd):                   #for문을 이용하여 반복
+        if r[y][x] == '*': continue               #if문을 이용해 r[y][x]가 *과 같다면
+        count = 0                                 #count 0으로 저장
+        c = [[''] if y - 1 < 0 else r[y-1][0 if x - 1 < 0 else x - 1:x+2],   #c에 리스트를 저장/if와 else를 통해 구현
+             r[y][0 if x - 1 < 0 else x - 1:x+2],                            #c에 리스트를 저장/if와 else를 통해 구현
+             [''] if y + 1 >= m else r[y+1][0 if x - 1 < 0 else x - 1:x+2]]  #c에 리스트를 저장/if와 else를 통해 구현
+        for z in c:                               #for문을 이용하여 반복
+            count+=z.count('*')                   #z에 *을 세서 더하고 count에 저장
+        r[y][x] = str(count)                      #count값을 문자형을 변환하여 저장
 
-print("output")
-for y in r:
-    print(''.join(y)) 
+print("output : ")                                #output 출력
+for y in r:                                       #for문으로 반복
+    print(''.join(y))                             #결과값 
 
 #----------------------------------------------
